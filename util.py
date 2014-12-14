b97aa62a4db052795ac98560ebabf926047c5a85
yuezhou.li <yuezhou.pro@gmail.com>
2014-12-14 18:36:25 +0800

    Update util.py

--- util.py
+++ util.py
@@ -4,9 +4,9 @@ from oss.oss_api import OssAPI
 
 def get_oss_api():
     try:
-        oss = OssAPI(host='oss-cn-qingdao-a.aliyuncs.com', 
-          access_id="uiUaNNz4uHKKX34j", 
-          secret_access_key='2w2d34QAvtnYaodyO3gOkIOZ1T0Hsg')
+        oss = OssAPI(host='host', 
+          access_id="accessid", 
+          secret_access_key='secret_key')
     except:
         return None
     return oss
